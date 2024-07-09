4c7f4944bafc7ae762e506f8b9257d6bb0a5f7c1
Lucas M. Braz <lmonteirobraz@gmail.com>
2024-07-09 16:00:40 -0300

    feat: show Kalshi logo in app bar

--- lib/score_calculator/input/view/score_calculator_input_page.dart
+++ lib/score_calculator/input/view/score_calculator_input_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ScoreCalculatorInputPage extends StatelessWidget {
   const ScoreCalculatorInputPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return  Scaffold(
+      appBar: AppBar(
+        title: SvgPicture.asset('assets/logo.svg', height: 20),
+      ),
+      body: const Center(
         child: Text('Score Calculator Input Page'),
       ),
     );

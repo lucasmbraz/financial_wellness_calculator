f2d556517a0024caea8f1248cb12c06e8c6fb5ec
Lucas M. Braz <lmonteirobraz@gmail.com>
2024-07-11 03:32:33 -0300

    feat: add field validation so values must be greater than zero

--- packages/design_system/lib/src/app_input_field.dart
+++ packages/design_system/lib/src/app_input_field.dart
@@ -44,8 +44,8 @@ class AppInputField extends StatelessWidget {
             ),
           ),
           validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter some text';
+            if (value == null || value.isEmpty || value.toDouble() == 0) {
+              return 'Please enter a value greater than zero';
             }
             return null;
           },
